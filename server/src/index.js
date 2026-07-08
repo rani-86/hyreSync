9a8ccdd0b9def5567b0e45fb85098e038670bccc
Rani <rani2003333@gmail.com>
2026-07-09 02:07:32 +0530

    feat: connect MongoDB Atlas via mongoose

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
+
+
+require('dotenv').config();
 const app = require('./app');
+const connectDB = require('./config/db');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`HireSync server running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`HireSync server running on port ${PORT}`);
+  });
 });
